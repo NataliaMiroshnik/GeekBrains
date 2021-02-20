fab251284095b6cbb7e3bd264cca76d2546b3cc9
NataliaMiroshnik <78311444+NataliaMiroshnik@users.noreply.github.com>
2021-02-20 20:47:36 +0300

    Delete task4.py

--- HW5/task4.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-with open('new_file4.txt', 'w') as write:
-    with open('file4.txt') as file:
-        text = file.read()
-    rename = Translator()
-    new_text = rename.translate(text)
-    print(new_text)
