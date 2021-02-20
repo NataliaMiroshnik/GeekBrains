0130c514d198f4ce45e53acb160296f6d877e709
NataliaMiroshnik <78311444+NataliaMiroshnik@users.noreply.github.com>
2021-02-20 20:48:04 +0300

    Delete task6.py

--- HW5/task6.py
+++
@@ -1,7 +0,0 @@
-subject = {}
-with open('file6.txt') as file:
-    for line in file:
-        name, hours = line.split(':')
-        name_sum = sum(map(int, "".join([i for i in hours if i == ' ' or (i >= '0' and i <= '9')]).split()))
-        subject[name] = name_sum
-    print(f"{subject}")
